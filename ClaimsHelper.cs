c8d0d4ad7c08de362da7dbf88a838d2caedfa570
stop-cran <stop-cran@list.ru>
2018-12-20 18:44:15 +0200

    Working sample.

---
+++ ClaimsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IdentityModel.Configuration;
+
+namespace AdfsWifSample
+{
+    public static class ClaimsHelper
+    {
+        public static string TrustedIssuer
+        {
+            get
+            {
+                return SystemIdentityModelSection.Current.IdentityConfigurationElements
+                    .GetElement("")
+                    .IssuerNameRegistry
+                    .ChildNodes[0]
+                    .SelectSingleNode("//authority")
+                    .Attributes["name"].Value;
+            }
+        }
+    }
+}
---
+++ Controllers/ClaimsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web.Http;
+
+namespace AdfsWifSample.Controllers
+{
+    [Authorize]
+    public class ClaimsController : ApiController
+    {
+        public string GetUserEmail()
+        {
+            return ((ClaimsPrincipal)Thread.CurrentPrincipal).Claims
+                .Single(claim => claim.Issuer == ClaimsHelper.TrustedIssuer && claim.Type == ClaimTypes.Email).Value;
+        }
+    }
+}
